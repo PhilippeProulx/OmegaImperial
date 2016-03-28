96f2f05c8898162f3eae379e27779b3b61a8d576
Philippe Proulx <philippe.php@gmail.com>
2016-03-28 10:05:39 +0200

    Use a set to create factories

--- Explorations/SVG-svg.js-Mouseover/main.js
+++ Explorations/SVG-svg.js-Mouseover/main.js
@@ -1,30 +1,27 @@
 var draw = SVG('board').size(300, 300);
 
-var factory = draw.polygon('0,0 30,0 30,60 90,30 90,60 150,30 150,100 0,100').fill('#800000').stroke({aligment:'outside', linejoin:'round', color: '#c00000', width: 4 });
+var factories = draw.set();
 
-var factory2 = factory.clone();
-
-factory.scale(0.5, 0.5).move(0, 0);
-factory2.scale(0.5, 0.5).move(128, 128);
-
-factory.mouseover(function()
+for (i = 0; i < 5; i++) 
 {
-  OnFactoryMouseover(this);
-})
+    factories.add(draw.polygon(''));
+}
 
-factory.mouseout(function()
+factories.each(function(i) 
 {
-  OnFactoryMouseout(this);
-})
-
-factory2.mouseover(function()
-{
-  OnFactoryMouseover(this);
-})
-
-factory2.mouseout(function()
-{
-  OnFactoryMouseout(this);
+  this.plot('0,0 30,0 30,60 90,30 90,60 150,30 150,100 0,100')
+    .fill('#800000')
+    .stroke({aligment:'outside', linejoin:'round', color: '#c00000', width: 4 })
+    .scale(0.5, 0.5)
+    .move(i * 64, i * 128)
+    .mouseover(function()
+    {
+      OnFactoryMouseover(this);
+    })
+    .mouseout(function()
+    {
+      OnFactoryMouseout(this);
+    });
 })
 
 function OnFactoryMouseover(factory)
