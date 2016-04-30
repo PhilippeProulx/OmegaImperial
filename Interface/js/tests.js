211db01c9e33300c516f288180cfdf8e6296cd33
Philippe Proulx <philippe.php@gmail.com>
2016-04-30 15:44:16 +0200

    Remove obselete test

--- Interface/js/tests.js
+++ Interface/js/tests.js
@@ -1,9 +1,6 @@
-
 function RunTests()
 {
   TestMainController()
-  //TestBoardMove()
-  //TestScoringTrack()
   //TestRegionSlots()
 }
 
@@ -27,40 +24,6 @@ function TestMainController()
   controller.SetPoints(Nation.France, 0)
   controller.SetPoints(Nation.Italy, 2)
   controller.SetPoints(Nation.GermanEmpire, 4)
-}
-
-function TestBoardMove()
-{
-  new Piece(PieceType.Flag, Nation.GreatBritain)
-  new Piece(PieceType.Marker, Nation.Italy)
-  new Piece(PieceType.Flag, Nation.France)
-  new Piece(PieceType.Flag, Nation.Russia)
-  new Piece(PieceType.Flag, Nation.GreatBritain)
-  new Piece(PieceType.Flag, Nation.GreatBritain)
-  new Piece(PieceType.Flag, Nation.GreatBritain)
-  new Piece(PieceType.Flag, Nation.GreatBritain)
-
-  Piece.all[0].Move(Region.Berlin)
-  Piece.all[1].Move(Region.Berlin)
-  Piece.all[2].Move(Region.Cologne)
-  Piece.all[3].Move(Region.Hamburg)
-  Piece.all[4].Move(Region.Holland)
-  Piece.all[5].Move(Region.Bordeaux)
-  Piece.all[6].Move(Region.Paris)
-  Piece.all[7].Move(Region.Hamburg)
-}
-
-function TestScoringTrack()
-{
-  // new Piece(PieceType.Marker, Nation.GreatBritain).Move(game.scoringTrack.spaces[0])
-  // new Piece(PieceType.Marker, Nation.AustriaHungary).Move(game.scoringTrack.spaces[0])
-  // new Piece(PieceType.Marker, Nation.Italy).Move(game.scoringTrack.spaces[0])
-  // new Piece(PieceType.Marker, Nation.France).Move(game.scoringTrack.spaces[0])
-  
-  // new Piece(PieceType.Marker, Nation.GreatBritain).Move(game.scoringTrack.spaces[1])
-  // new Piece(PieceType.Marker, Nation.AustriaHungary).Move(game.scoringTrack.spaces[1])
-  // new Piece(PieceType.Marker, Nation.Italy).Move(game.scoringTrack.spaces[1])
-  // new Piece(PieceType.Marker, Nation.France).Move(game.scoringTrack.spaces[1])
 }
 
 function TestRegionSlots()
